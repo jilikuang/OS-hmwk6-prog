99631e143889f09828512e80959658eee614f712
jh3478 <jh3478@columbia.edu>
2014-11-28 09:29:21 -0500

    implement sys call

--- flo-kernel/include/linux/gps.h
+++ flo-kernel/include/linux/gps.h
@@ -2,8 +2,16 @@
  * Header file of GPS location
  */
 
+/* sent from user space */
 struct gps_location {
 	double latitude;
 	double longitude;
 	float accuracy;	/* In meter */
+};
+
+struct gps_kdata {
+	unsigned char m_lat[8];	/* represent 64 bit double 	*/
+	unsigned char m_lon[8];	/* represent 64 bit double 	*/	
+	unsigned char m_acc[4];	/* represent 32 bit float 	*/
+	unsigned char m_age[4];	/* represent 32 bit integer	*/
 };
--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -4,10 +4,56 @@
 
 #include <linux/syscalls.h>
 #include <linux/gps.h>
+#include <linux/rwlock.h>
+#include <linux/uaccess.h>
+
+/* debug define */
+#define log	printk
+
+/* my own repository */
+static struct gps_kdata s_kdata;
+static DEFINE_RWLOCK(s_lock);
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 {
 	long retval = 0;
+	unsigned long sz = sizeof(struct gps_location);
+	struct gps_location kloc;
 
+	/* safety check */
+	if (loc == NULL)
+		return -EINVAL;
+
+	if (copy_from_user(&kloc, loc, sz) != sz) {
+		log("copy_from_user failure\n");
+		return -EINVAL;
+	} 
+
+	write_lock(&s_lock);
+
+	memcpy (s_kdata.m_lat, &loc->latitude, sizeof(double));
+	memcpy (s_kdata.m_lon, &loc->longitude, sizeof(double));
+	memcpy (s_kdata.m_acc, &loc->accuracy, sizeof(float));
+
+#if 0
+	s_kdata.m_age = /* get current time */
+#endif
+
+	write_unlock(&s_lock);
 	return retval;
 }
+
+void get_gps_data(struct gps_kdata *data)
+{
+	read_lock(&s_lock);
+	
+	memcpy (data->m_lat, s_kdata.m_lat, sizeof(double));
+	memcpy (data->m_lon, s_kdata.m_lon, sizeof(double));
+	memcpy (data->m_age, s_kdata.m_acc, sizeof(float));
+
+#if 0
+	s_kdata.m_age = /* get current time */
+#endif
+	read_unlock(&s_lock);
+}
+
