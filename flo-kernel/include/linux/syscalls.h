26455594c403ce20e3ca86d951f0d1767c97ef1d
Bo Yin <by2223@columbia.edu>
2014-11-29 18:35:56 -0500

    add get_gps_location as syscall 379

--- flo-kernel/include/linux/syscalls.h
+++ flo-kernel/include/linux/syscalls.h
@@ -859,5 +859,6 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 				      unsigned long riovcnt,
 				      unsigned long flags);
 asmlinkage long sys_set_gps_location(struct gps_location __user *loc);
+asmlinkage long sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc);
 
 #endif
